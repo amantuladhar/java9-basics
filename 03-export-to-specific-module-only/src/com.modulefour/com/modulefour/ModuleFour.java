1f8ef9711a0877b016c3f142ffce1264e3a70f36
Aman Tuladhar <Aman.Tuladhar@pointclickcare.com>
2017-10-04 19:02:59 -0500

    Info to export to different modules

---
+++ 03-export-to-specific-module-only/src/com.modulefour/com/modulefour/ModuleFour.java
@@ -0,0 +1,8 @@
+package com.modulefour;
+
+import com.moduleone.ModuleOne;
+public class ModuleFour {
+    public static void main(String[] args){
+        ModuleOne.callMe();
+    }
+}
---
+++ 03-export-to-specific-module-only/src/com.modulefour/module-info.java
@@ -0,0 +1,3 @@
+module com.modulefour{
+    requires com.moduleone;
+}
